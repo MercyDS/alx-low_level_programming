bc9cc8da442c98f06e7d1ef087775a01dc3f8635
MercyDS <ezehe3217@gmail.com>
2023-08-11 21:12:14 -0700

    correction to 1 to 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
-*main : print a string                                                               
+*main - print a string
 *
-*return : always (0) for successful run
+*Return: always (0) for successful run
 */
 
 int main(void)
 {
-        puts("programming is like building a multilingual puzzle");
+	puts("programming is like building a multilingual puzzle");
 
-        return (0);
+	return (0);
 }
+
