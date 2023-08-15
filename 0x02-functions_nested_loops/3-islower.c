176ba35e8b9253bf4a52e9d2aca55cd210a51b11
MercyDS <ezehe3217@gmail.com>
2023-08-15 12:16:36 -0700

    correct 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-
+/**
+ *_islower - library to main function
+ *
+ *Description: checks if for lower case and return 0 if upper case
+ *
+ *@c: int variable(argument) a character
+ *Return: always(0) for success
+ */
 
 int _islower(int c)
 {
-int a;
+	if (c > 65 && c < 90)
+		return (0);
 
-if (c > 65 && c < 90)
-return 0;
-
-else
-return 1;
+	else
+		return (1);
 }
