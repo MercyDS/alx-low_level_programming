d9500acca3dd381b1a457a8f9a3b3a693cfd23de
MercyDS <ezehe3217@gmail.com>
2023-08-15 18:28:50 -0700

    back to the previous code (9)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,16 +14,15 @@ void times_table(void)
 			product = row * column;
 
 			if (column != 0)
-				putchar(',');
+				_putchar(',');
 
+			if (product < 10 && column != 0)
 				_putchar(' ');
 
-				if (product < 10)
-					_putchar(' ');
+			if (product >= 10)
+				_putchar(product / 10 + '0');
 
-					if (product >= 10)
-						_putchar(product / 10 + '0');
-						_putchar(product % 10 + '0');
+			_putchar(product % 10 + '0');
 		}
 		_putchar('\n');
 	}
