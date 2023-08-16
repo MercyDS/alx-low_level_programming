9deaa47aa1e72bdb2c6823a2c5fd2d502eda9741
MercyDS <ezehe3217@gmail.com>
2023-08-15 18:22:42 -0700

    correct 9 (so as to include tab in output)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,15 +14,16 @@ void times_table(void)
 			product = row * column;
 
 			if (column != 0)
-				_putchar(',');
+				putchar(',');
 
-			if (product < 10 && column != 0)
 				_putchar(' ');
 
-			if (product >= 10)
-				_putchar(product / 10 + '0');
+				if (product < 10)
+					_putchar(' ');
 
-			_putchar(product % 10 + '0');
+					if (product >= 10)
+						_putchar(product / 10 + '0');
+						_putchar(product % 10 + '0');
 		}
 		_putchar('\n');
 	}
