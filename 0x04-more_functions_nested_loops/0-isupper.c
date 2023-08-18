7ce2520a47f7a015a5b5a28d016a9e25be297ec4
MercyDS <ezehe3217@gmail.com>
2023-08-18 09:39:02 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_isupper - check for uppercase
+ *
+ *Return: (1) for uppercase otherwise (0)
+ */
+
+int _isupper(int c)
+{
+	c;
+
+	if (c >= 64 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
