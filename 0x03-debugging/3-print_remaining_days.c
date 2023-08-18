d340e220b0d8f5cc229630d8a56a1c6114741f52
MercyDS <ezehe3217@gmail.com>
2023-08-18 07:34:59 -0700

    correct 'print_remaining_days()function'

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60)
 		{
-			/*day++;*/
+			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
@@ -31,9 +31,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day - 1); /*add -1 to*/
-								  /*day*/
-			printf("Remaining days: %d\n", 365 - (day - 1));
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
