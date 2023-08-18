7901c13ee80f7d1e158b4d5916132d22d035d41d
MercyDS <ezehe3217@gmail.com>
2023-08-18 05:01:34 -0700

    comment out correctly the wrong line in 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /*This will always be true
-			so code will run infinitely*/
+	/*while (i < 10)
 	{
 		putchar(i);
-	}
+		}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
