9a53419695c9cf283097c7fa0bdce06ea4297612
MercyDS <ezehe3217@gmail.com>
2023-08-24 15:45:48 -0700

    betty compliance of 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,11 +14,13 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
+	j = 0;
+
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j = 0;
+
 	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
