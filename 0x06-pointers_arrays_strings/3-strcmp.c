b666447d291e1944d871c58e7449cc08b823859d
MercyDS <ezehe3217@gmail.com>
2023-08-24 16:32:02 -0700

    change entire 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,21 @@
 #include <string.h>
 
 /**
-* _strcmp - works like the standard strcmp function
+* _strcmp - compare string 1 and 2
 *
-* @s1: first string
-* @s2: second string
-* Return: (0) if s1 and s2 are same or the
-* difference if not
+* @s1: first sstring (pointer to char)
+* @s2: second string (pointer to char)
+* Return: (0) or (difference)
 */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	for (i = 0; i < strlen(s1); i++)
+	for (i = 0; i < strlen(s1) && s1[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			return ((int)(s1[i] - s2[i]));
+			return ((int)s1[i] - (int)s2[i]);
 		}
 	}
 	return (0);
