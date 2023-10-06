879138219ea5b7df0f251e52a48cd12f35bb3869
MercyDS <https://github.com/MercyDS>
2023-10-05 17:30:06 -0700

    to stderr instead of stdout in 101*

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- *main - prints a string and a newline to to standard error
+ *main - prints a string and a newline to standard error
  *
  *Return: always(0) successful
  */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	     stdout);
+	     stderr);
+	
 	return (0);
 }
